191ee8ccd96a4a345deb2bf1d39d6caa19c12d46
Nicholas Morano <nicholasamorano@gmail.com>
2023-02-08 15:52:45 +0100

    update

--- src/ColourCount.jsx
+++ src/ColourCount.jsx
@@ -1,8 +1,27 @@
 import { createElement } from "react";
+import JacketSvgIcon1 from "../components/JacketSvgIcon1";
+import JacketSvgIcon2 from '../components/JacketSvgIcon2';
 
 import { HelloWorldSample } from "./components/HelloWorldSample";
 import "./ui/ColourCount.css";
 
 export function ColourCount({ sampleText }) {
-    return <HelloWorldSample sampleText={sampleText} />;
+    return (
+        <div className="App">
+          <header className="App-header">
+            <JacketSvgIcon2 />
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </header>
+        </div>
+      );
 }
